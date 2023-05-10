59cccf489fca72cea331d3a02b534ca9fa35d59a
tanupa <tanupa01@gmail.com>
2023-05-10 03:35:45 -0700

    updated createPost.dart to add heart and comment icon

--- lib/view/createPost.dart
+++ lib/view/createPost.dart
@@ -25,24 +25,18 @@ class _CreatePostState extends State<CreatePost> {
   }
 
   void createPost() async {
-    // Get the values from the text fields
     String caption = captionController.text;
     String link = linkController.text;
 
-    // Make an HTTP request to your API endpoint to create a new post
-    // Replace 'your-api-endpoint' with the actual URL of your API
-    Uri url = Uri.parse('https://your-api-endpoint/create-post');
+    Uri url = Uri.parse('ADD URL HERE');
     final response = await http.post(url, body: {
       'caption': caption,
       'link': link,
     });
 
-    // Check the response status
     if (response.statusCode == 200) {
-      // Post created
       Navigator.pop(context);
     } else {
-      // An error occurred
       print('Error creating post: ${response.body}');
     }
   }
@@ -50,77 +44,86 @@ class _CreatePostState extends State<CreatePost> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(
-            Icons.home,
-            color: Colors.black,
-          ),
-          onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const HomePage()));
-          },
-        ),
-        title: const Text(
-          "Create Post",
-          style: TextStyle(fontSize: 23, color: Colors.black),
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.fitness_center),
-            color: Colors.black,
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const MyWorkouts()));
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.restaurant),
-            color: Colors.black,
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const Calorie()));
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.account_circle),
-            color: Colors.black,
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const Accounts()));
-            },
-          ),
-        ],
-      ),
-      body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-       Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: captionController,
-              decoration: const InputDecoration(
-                labelText: 'Caption',
-              ),
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(
+              Icons.home,
+              color: Colors.black,
             ),
-            const SizedBox(height: 16.0),
-            TextField(
-              controller: linkController,
-              decoration: const InputDecoration(
-                labelText: 'Link',
-              ),
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => const HomePage()));
+            },
+          ),
+          title: const Text(
+            "Create Post",
+            style: TextStyle(fontSize: 23, color: Colors.black),
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.favorite),
+              color: Colors.red,
+              onPressed: () {
+              },
             ),
-            const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: createPost,
-              child: const Text('Post'),
+            IconButton(
+              icon: const Icon(Icons.comment),
+              color: Colors.grey,
+              onPressed: () {
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.fitness_center),
+              color: Colors.black,
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const MyWorkouts()));
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.restaurant),
+              color: Colors.black,
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const Calorie()));
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.account_circle),
+              color: Colors.black,
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const Accounts()));
+              },
             ),
           ],
         ),
-      ),
-    ]));
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: captionController,
+                      decoration: const InputDecoration(
+                        labelText: 'Caption',
+                      ),
+                    ),
+                    const SizedBox(height: 16.0),
+                    TextField(
+                      controller: linkController,
+                      decoration: const InputDecoration(
+                        labelText
+                            : 'Link',
+                      ),
+                    ),
+                    const SizedBox(height: 16.0),
+                    ElevatedButton(
+                      onPressed: createPost,
+                      child: const Text('Post'),
+                    ),
+                  ],
+                ),
+              ),
+            ]));
   }
 }
